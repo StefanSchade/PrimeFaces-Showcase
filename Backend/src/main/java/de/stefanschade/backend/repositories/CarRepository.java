3a9ac0eb6646aa29292071e9949ce4e243ef1ed3
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-03-26 11:23:38 +0100

    setup backend with h2 database

---
+++ Backend/src/main/java/de/stefanschade/backend/repositories/CarRepository.java
@@ -0,0 +1,15 @@
+package de.stefanschade.backend.repositories;
+
+import de.stefanschade.backend.domain.CarEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CarRepository extends CrudRepository<CarEntity,Long> {
+
+    @Override
+    Optional<CarEntity> findById(Long aLong);
+
+}
