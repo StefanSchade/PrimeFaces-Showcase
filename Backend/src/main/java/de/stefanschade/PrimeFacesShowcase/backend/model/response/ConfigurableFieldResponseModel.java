549f2b16f03620f3ccbb5a6e5eb6367b44a43c57
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-03-30 18:34:07 +0200

    get working but flawed

---
+++ Backend/src/main/java/de/stefanschade/PrimeFacesShowcase/backend/model/response/ConfigurableFieldResponseModel.java
@@ -0,0 +1,16 @@
+package de.stefanschade.PrimeFacesShowcase.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConfigurableFieldDto {
+
+    private String fieldName;
+    private FieldType fieldType;
+}
