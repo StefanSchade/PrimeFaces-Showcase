82bc5e4b850c8a0fe0a6f3d56e6f8ef8a48c4812
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-04-03 15:50:55 +0200

    deployproblem

--- Frontend/src/main/java/de/stefanschade/PrimeFacesShowcase/frontend/BasicView.java
+++ Frontend/src/main/java/de/stefanschade/PrimeFacesShowcase/frontend/BasicView.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 import java.util.List;
 
 @Log
-@Named("dtBasicView")
+@Named
 @ViewScoped
 public class BasicView implements Serializable {
 
--- Frontend/src/main/java/de/stefanschade/PrimeFacesShowcase/frontend/ProductTemplateService.java
+++ Frontend/src/main/java/de/stefanschade/PrimeFacesShowcase/frontend/ProductTemplateService.java
@@ -4,13 +4,6 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Named;
-import javax.json.JsonArray;
-import javax.json.JsonString;
-import javax.json.JsonValue;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,16 +15,16 @@ public class ProductTemplateService {
     public List<ProductTemplate> retrieveTemplates() {
 
         List<ProductTemplate> returnValue = new ArrayList<>();
-        log.info("Testing");
+        System.out.println("Testing");
         returnValue.add(new ProductTemplate("testtemplate", null));
 
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("http://localhost:8082/producttemplate");
-        JsonArray response = target.request(MediaType.APPLICATION_JSON).get(JsonArray.class);
-        for (JsonValue currentelement : response) {
-            System.out.println("XXXX " + currentelement);
-            returnValue.add(((JsonString) currentelement).getString());
-        }
+//        Client client = ClientBuilder.newClient();
+//        WebTarget target = client.target("http://localhost:8082/producttemplate");
+//        JsonArray response = target.request(MediaType.APPLICATION_JSON).get(JsonArray.class);
+//        for (JsonValue currentelement : response) {
+//            System.out.println("XXXX " + currentelement);
+//            returnValue.add(((JsonString) currentelement).getString());
+//        }
         return returnValue;
     }
 
