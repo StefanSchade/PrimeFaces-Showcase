4e6903761437c5c87e444f193bfe88d654335bb5
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-04-18 08:59:34 +0200

    rest client with jersey

--- frontend/src/main/java/de/stefanschade/primefacesshowcase/frontend/rest/BackendRestClient.java
+++ frontend/src/main/java/de/stefanschade/primefacesshowcase/frontend/rest/BackendRestClient.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
@@ -14,7 +16,10 @@ import java.util.List;
 @ApplicationScoped
 public class BackendRestClient {
 
-    public List<ProductTemplate> productTemplates() {
+    private final String BASE_URI =
+            "http://localhost:8082";
+
+    public List<ProductTemplate> productTemplates1() {
         Client client = ClientBuilder.newBuilder().build();
         WebTarget target = client.target("http://localhost:8082/producttemplate");
 
@@ -26,5 +31,24 @@ public class BackendRestClient {
         log.info("backend returned {} product templates", productTemplates.size());
 
         return productTemplates;
+
+    }
+
+    public List<ProductTemplate> productTemplates() {
+
+
+        Client client = javax.ws.rs.client.ClientBuilder.newClient();
+
+        WebTarget  webTarget = client.target(BASE_URI);
+        WebTarget resource = webTarget.path("producttemplate");
+        javax.ws.rs.client.Invocation.Builder builder=resource.
+                request(javax.ws.rs.core.MediaType.APPLICATION_JSON);
+        Invocation invocation=builder.buildGet();
+        GenericType responseType=new GenericType<List<ProductTemplate>>() { };
+        List<ProductTemplate> productTemplates = (List<ProductTemplate>) invocation.invoke(responseType);
+        log.info("backend returned {} product templates", productTemplates.size());
+
+        return productTemplates;
+
     }
 }
