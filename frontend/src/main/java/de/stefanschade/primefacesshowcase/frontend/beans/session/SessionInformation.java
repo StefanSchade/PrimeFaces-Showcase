c012bfbc98e324263e3c05062c21b396b7301e57
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-05-06 17:18:38 +0200

    index as landing page

---
+++ frontend/src/main/java/de/stefanschade/primefacesshowcase/frontend/beans/session/SessionInformation.java
@@ -0,0 +1,24 @@
+package de.stefanschade.primefacesshowcase.frontend.beans.session;
+
+import lombok.Data;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Data
+@Named
+@SessionScoped
+public class SessionInformation implements Serializable  {
+
+    private String resultFlow1;
+    private String resultFlow2;
+    private String resultTemplateBrowser;
+
+    @PostConstruct
+    public void init() {
+        resultTemplateBrowser = "Template Browser not called yet";
+    }
+
+}
--- frontend/src/main/java/de/stefanschade/primefacesshowcase/frontend/beans/view/ProductTemplateView.java
+++ frontend/src/main/java/de/stefanschade/primefacesshowcase/frontend/beans/view/ProductTemplateView.java
@@ -2,10 +2,12 @@ package de.stefanschade.primefacesshowcase.frontend.beans.view;
 
 import de.stefanschade.primefacesshowcase.frontend.beans.entities.ConfigurableField;
 import de.stefanschade.primefacesshowcase.frontend.beans.entities.ProductTemplate;
+import de.stefanschade.primefacesshowcase.frontend.beans.session.SessionInformation;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.annotation.PreDestroy;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -19,10 +21,24 @@ import java.io.Serializable;
 public class ProductTemplateView implements Serializable {
 
     @Inject
+    private SessionInformation sessionInformation;
+
+    @Inject
     private FieldTable fieldTable;
 
     @Inject
     private TemplateTable templateTable;
+
+    @PreDestroy
+    public void writeResults() {
+        ProductTemplate productTemplate = templateTable.getTemplateSelected();
+
+        if (productTemplate == null) {
+            sessionInformation.setResultTemplateBrowser("Keine Auswahl");
+        } else {
+            sessionInformation.setResultTemplateBrowser("Selected "+ productTemplate.getTemplatename());
+        }
+    }
 
     public void templateDetailsButtonClicked(ProductTemplate template) {
         templateTable.selectTemplate(template);
@@ -54,4 +70,5 @@ public class ProductTemplateView implements Serializable {
     public void fieldsDetailsButtonClicked(ConfigurableField field) {
         fieldTable.selectFieldDetails(field);
     }
+
 }
