914cfb906ee3d10c9540ac8689e19faef91479d2
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-05-13 03:57:29 +0200

    MapStruct Test

--- backend/src/test/java/de/stefanschade/primefacesshowcase/backend/mapper/ProductTemplateMapperTest.java
+++ backend/src/test/java/de/stefanschade/primefacesshowcase/backend/mapper/ProductTemplateMapperTest.java
@@ -14,16 +14,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProductTemplateMapperTest {
 
+    public static final String NAME_TEMPLATE = "template";
+    public static final String NAME_FIELD_1 = "field1";
+    public static final String NAME_FIELD_2 = "field2";
+    public static final Integer NR_OF_FIELDS = Integer.valueOf(2);
+
     ProductTemplateMapper productTemplateMapper = ProductTemplateMapper.INSTANCE;
 
     @Test
     public void productTemplateEntityToProductTemplateDto() throws Exception {
 
         //given
-        ProductTemplateEntity productTemplateEntity = new ProductTemplateEntity("template");
-        ConfigurableFieldEntity configurableFieldEntity1 = new ConfigurableFieldEntity(productTemplateEntity, "field1", FieldType.DOUBLE);
-        ConfigurableFieldEntity configurableFieldEntity2 = new ConfigurableFieldEntity(productTemplateEntity, "field2", FieldType.STRING);
-        List<ConfigurableFieldEntity> configurableFieldEntityList = new ArrayList<>(2);
+        ProductTemplateEntity productTemplateEntity = new ProductTemplateEntity(NAME_TEMPLATE);
+        ConfigurableFieldEntity configurableFieldEntity1 = new ConfigurableFieldEntity(productTemplateEntity, NAME_FIELD_1, FieldType.DOUBLE);
+        ConfigurableFieldEntity configurableFieldEntity2 = new ConfigurableFieldEntity(productTemplateEntity, NAME_FIELD_2, FieldType.STRING);
+        List<ConfigurableFieldEntity> configurableFieldEntityList = new ArrayList<>(NR_OF_FIELDS);
         configurableFieldEntityList.add(configurableFieldEntity1);
         configurableFieldEntityList.add(configurableFieldEntity2);
         productTemplateEntity.setFields(configurableFieldEntityList);
@@ -32,9 +37,9 @@ class ProductTemplateMapperTest {
         ProductTemplateDto productTemplateDto = productTemplateMapper.productTemplateEntityToProductTemplateDto(productTemplateEntity);
 
         //then
-        assertEquals("template", productTemplateDto.getTemplatename());
-        assertEquals(Integer.valueOf(2), productTemplateDto.getFieldsdto().size());
-        assertEquals("field1",productTemplateDto.getFieldsdto().get(0).getFieldname());
+        assertEquals(NAME_TEMPLATE, productTemplateDto.getTemplatename());
+        assertEquals(NR_OF_FIELDS, productTemplateDto.getFieldsdto().size());
+        assertEquals(NAME_FIELD_1,productTemplateDto.getFieldsdto().get(0).getFieldname());
 
     }
 }
